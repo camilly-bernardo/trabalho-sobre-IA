84611c13a971f6247eaf64260080ee1f6cbb3316
camilly-bernardo <127161037+camilly-bernardo@users.noreply.github.com>
2024-06-26 20:11:15 -0300

    jtyujy

---
+++ script.js
@@ -0,0 +1,26 @@
+const caixaprincipal = document. queryselector (" caixa-principal");
+const caixaPerguntas = document. queryselector (" caixa-perguntas");
+const caixaAlternativas = document. queryselector (" caixa-alternativas");
+const caixaResultado = document. queryselector (" caixa-resultado"):
+const textoResultado = document. queryselector (" texto-resultado");
+
+const perguntas = [
+    { 
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realista. Qual o primeiro pensamento?”,
+        alternativas: [
+{
+texto: "Isso é assustador!",
+afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. “
+},
+{
+texto: "Isso é maravilhoso!"
+afirmacao: "Quis saber como usar IA no seu dia a dia.”
+}
+]
+},
+{
+enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso da IA em sala de aula. Qual atitude você toma?”,
+alternativas: [
+{
+texto: "utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.”,
+afirmacao: "Conseguiu utilizar a IA para buscar informações úteis.
